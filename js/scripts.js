076c2dfbad3af357a7b262b106a32fab31de2c63
“SivonneWard” <sivonne@live.unc.edu>
2018-11-08 11:09:52 -0500

    got hard-coded table in the page, need it to look more like DataTables so working on fixing the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,121 +1,74 @@
 $(function(){
   console.log('scripts loaded');
+  //THE BASIC HIGHCHART PASTE JOB WITH HARD-CODED ARRAY
+   // var myChart = Highcharts.chart('#basic-table', {
+   //        chart: {
+   //            type: 'bar'
+   //        },
+   //        title: {
+   //            text: 'Fruit Consumption'
+   //        },
+   //        subtitle:{
+   //          text: 'Fucking Love Fruit'
+   //        },
+   //        xAxis: {
+   //            categories: ['Apples', 'Bananas', 'Oranges']//NOTICE THIS IS AN ARRAY
+   //        },
+   //        yAxis: {
+   //            title: {
+   //                text: 'Fruit eaten'
+   //            }
+   //        },
+   //        series: [{
+   //            name: 'Jane',//DELETE THIS AND SEE HOW THE CHART CHANGES
+   //            data: [1, 0, 4]//HOVER OVER THE BARS TO SEE WHAT THESE NUMBERS REPRESENT
+   //        }, {
+   //            name: 'John',
+   //            data: [5, 7, 3]
+   //        }]//ADD ANOTHER OBJECT AND SEE HOW IT CHANGES
+   //    });
+// $('#ajax-table').DataTable({
+//     "ajax": {
+//       "url": "/txt/grads.txt",
+//       "dataSrc": ''
+//   },
+//     "columns": [
+//       {"data": "Year"},
+//       {"data":"University Name"},
+//       {"data":"Grads Total"}
+//     ],
+//     "columnDefs":[{
+//        "targets":[0, 3],
+//        "createdCell": function(td, cellData, rowData, row, col){
+//          if(cellData > 5){
+//            $(td).css('color', 'red');
+//          }//close if statement
+//          $(td).on({
+//            mouseenter: function(){
+//              var txt = $(this).text();
+//              $('#' + txt).toggleClass('hidden');
+//            },
+//            mouseleave: function(){
+//              var txt = $(this).text();
+//              $('#' + txt).toggleClass('hidden');
+//            }
+//
+//          });//close on method
+//
+//
+//
+//        }//close createdCell
+//
+//     }]//close columnDefs
+//
+//
+//
+// });//close dataTable
+//
 
-
-/*
-$('#ajax-table').DataTable({
-    "ajax": {
-      "url": "/txt/grads.txt",
-      "dataSrc": ''
-  },
-    "columns": [
-      {"data": "Year"},
-      {"data":"University Name"},
-      {"data":"Grads Total"}
-    ],
-    "columnDefs":[{
-       "targets":[0, 3],
-       "createdCell": function(td, cellData, rowData, row, col){
-         if(cellData > 5){
-           $(td).css('color', 'red');
-         }//close if statement
-         $(td).on({
-           mouseenter: function(){
-             var txt = $(this).text();
-             $('#' + txt).toggleClass('hidden');
-           },
-           mouseleave: function(){
-             var txt = $(this).text();
-             $('#' + txt).toggleClass('hidden');
-           }
-
-         });//close on method
-
-
-
-       }//close createdCell
-
-    }]//close columnDefs
-
-
-
-});//close dataTable
-
-*/
 
 var url = '/js/unisAcrossCountry.json';
 var data = [];
-
-// $.ajax({
-//   type: 'GET',
-//   dataType: "json",
-//   url: url,
-//   data: data,
-//   success: function(data) {
-//     // Instantiate the map
-//     Highcharts.mapChart('container', {
-//       //Initialize variables
-//
-//      chart: {
-//          map: 'countries/us/us-all',
-//          borderWidth: 1
-//      },
-//
-//      title: {
-//          text: 'AASPs by State'
-//      },
-//
-//      exporting: {
-//          sourceWidth: 600,
-//          sourceHeight: 500
-//      },
-//
-//      legend: {
-//          layout: 'horizontal',
-//          borderWidth: 0,
-//          backgroundColor: 'rgba(255,255,255,0.85)',
-//          floating: true,
-//          verticalAlign: 'top',
-//          y: 25
-//      },
-//
-//      mapNavigation: {
-//          enabled: true
-//      },
-//
-//      colorAxis: {
-//          min: 1,
-//          type: 'logarithmic',
-//          minColor: '#EEEEFF',
-//          maxColor: '#000022',
-//          stops: [
-//              [0, '#EFEFFF'],
-//              [0.67, '#4444FF'],
-//              [1, '#000022']
-//          ]
-//      },
-//
-//      series: [{
-//          animation: {
-//              duration: 1000
-//          },
-//          data: data,
-//          joinBy: ['postal-code', 'code'],
-//          dataLabels: {
-//              enabled: true,
-//              color: '#FFFFFF',
-//              format: '{point.uni-name}'
-//          },
-//          name: 'University Information',
-//          tooltip: {
-//              pointFormat: '{point.uni-name}: {point.awards}'
-//          }
-//      }]
-//
-//     });
-//   }
-// });
-
 //High Maps JS
 $.getJSON('/js/stateunis.json', function (data) {
    // Make codes uppercase to match the map data
@@ -131,7 +84,7 @@ Highcharts.mapChart('container', {
      borderWidth: 1
  },
  title: {
-     text: 'Asian-American Studies Programs by State'
+     text: 'Asian-American Studies by State'
  },
 
  exporting: {
