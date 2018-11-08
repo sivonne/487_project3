1c79dcf584ced2845af95783894c5fff3bf49af6
“SivonneWard” <sivonne@live.unc.edu>
2018-11-07 20:18:18 -0500

    tried getting dataTables to work, need to hard-code it probably. Working on high Maps, got a map to show up without color coding. CSS is mainly decided on for the rest of the document.

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,21 @@ $(function(){
   console.log('scripts loaded');
 
 
-$('#hard-coded-table').DataTable();
-
+/*
 $('#ajax-table').DataTable({
-    "ajax": "/js/nations.txt",
+    "ajax": {
+      "url": "/txt/grads.txt",
+      "dataSrc": ''
+  },
     "columns": [
-      {"data": "name"},
-      {"data":"Area"},
-      {"data":"Population"},
-      {"data":"Airports"}
+      {"data": "Year"},
+      {"data":"University Name"},
+      {"data":"Grads Total"}
     ],
     "columnDefs":[{
        "targets":[0, 3],
        "createdCell": function(td, cellData, rowData, row, col){
-         if(cellData > 400){
+         if(cellData > 5){
            $(td).css('color', 'red');
          }//close if statement
          $(td).on({
@@ -40,5 +41,78 @@ $('#ajax-table').DataTable({
 
 });//close dataTable
 
+*/
+
+
+
+
+//High Maps JS
+$.getJSON('js/unisAcrossCountry.json', function (data) {
+
+    // Make codes uppercase to match the map data
+    /*$.each(data, function () {
+        this.code = this.code.toUpperCase();
+    });
+*/
+    // Instantiate the map
+    Highcharts.mapChart('container', {
+
+        chart: {
+            map: 'countries/us/us-all',
+            borderWidth: 1
+        },
+
+        title: {
+            text: 'Asian-American Studies Programs by State'
+        },
+
+        exporting: {
+            sourceWidth: 600,
+            sourceHeight: 500
+        },
+
+        legend: {
+            layout: 'horizontal',
+            borderWidth: 0,
+            backgroundColor: 'rgba(255,255,255,0.85)',
+            floating: true,
+            verticalAlign: 'top',
+            y: 25
+        },
+
+        mapNavigation: {
+            enabled: true
+        },
+
+        colorAxis: {
+            min: 1,
+            type: 'logarithmic',
+            minColor: '#EEEEFF',
+            maxColor: '#000022',
+            stops: [
+                [0, '#EFEFFF'],
+                [0.67, '#4444FF'],
+                [1, '#000022']
+            ]
+        },
+
+        series: [{
+            animation: {
+                duration: 1000
+            },
+
+            joinBy: ['postal-code', 'code'],
+            dataLabels: {
+                enabled: true,
+                color: '#FFFFFF',
+                format: '{point.code}'
+            },
+            name: 'Asian-American Studies Programs by State',
+            tooltip: {
+                pointFormat: '{point.code}: {point.uni-name}'
+            }
+        }]
+    });
+});
 
 });
