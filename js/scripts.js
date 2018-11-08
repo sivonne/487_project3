8d62a4395713c8e4e5f135b278fc5a62a6fa4c30
“SivonneWard” <sivonne@live.unc.edu>
2018-11-08 08:57:12 -0500

    fixed High Maps so that the states aligned with their actual correct acronymns, have tauCharts working somewhat, need to add table next

--- js/scripts.js
+++ js/scripts.js
@@ -118,76 +118,18 @@ var data = [];
 
 //High Maps JS
 $.getJSON('/js/stateunis.json', function (data) {
+   // Make codes uppercase to match the map data
+      $.each(data, function () {
+          this.code = this.code.toUpperCase();
+      });
+      // Instantiate the map
 
-// // Instantiate the map
-// Highcharts.mapChart('container', {
-//   //Initialize variables
-//
-//  chart: {
-//      map: 'countries/us/us-all',
-//      borderWidth: 1
-//  },
-//
-//  title: {
-//      text: 'AASPs by State'
-//  },
-//
-//  exporting: {
-//      sourceWidth: 600,
-//      sourceHeight: 500
-//  },
-//
-//  legend: {
-//      layout: 'horizontal',
-//      borderWidth: 0,
-//      backgroundColor: 'rgba(255,255,255,0.85)',
-//      floating: true,
-//      verticalAlign: 'top',
-//      y: 25
-//  },
-//
-//  mapNavigation: {
-//      enabled: true
-//  },
-//
-//  colorAxis: {
-//      min: 1,
-//      type: 'logarithmic',
-//      minColor: '#EEEEFF',
-//      maxColor: '#000022',
-//      stops: [
-//          [0, '#EFEFFF'],
-//          [0.67, '#4444FF'],
-//          [1, '#000022']
-//      ]
-//  },
-//
-//  series: [{
-//      animation: {
-//          duration: 1000
-//      },
-//      data: data,
-//      joinBy: ['postal-code', 'code'],
-//      dataLabels: {
-//          enabled: true,
-//          color: '#FFFFFF',
-//          format: '{point.code}'
-//      },
-//      name: 'University Information',
-//      tooltip: {
-//          pointFormat: '{point.uni-name}: {point.awards}'
-//      }
-//  }]
-
-// Instantiate the map
 Highcharts.mapChart('container', {
   //Initialize variables
-
  chart: {
      map: 'countries/us/us-all',
      borderWidth: 1
  },
-
  title: {
      text: 'AASPs by State'
  },
@@ -216,9 +158,9 @@ Highcharts.mapChart('container', {
      minColor: '#EEEEFF',
      maxColor: '#000022',
      stops: [
-         [0, '#EFEFFF'],
-         [0.67, '#4444FF'],
-         [1, '#000022']
+       [0, '#EFEFFF'],
+       [0.67, '#4444FF'],
+       [1, '#000022']
      ]
  },
 
@@ -227,13 +169,13 @@ Highcharts.mapChart('container', {
          duration: 1000
      },
      data: data,
-     joinBy: null,
+     joinBy: ['postal-code', 'code'],
      dataLabels: {
          enabled: true,
          color: '#FFFFFF',
          format: '{point.code}'
      },
-     name: 'University Information',
+     name: 'Universities Offering Asian American Majors',
      tooltip: {
          pointFormat: '{point.code}: {point.university}'
      }
@@ -241,16 +183,89 @@ Highcharts.mapChart('container', {
 
 });
 });
+
+
+var url = 'js/demographics-reformatted.json';
+var demographics = '';
+
+$.ajax({
+type:'GET',
+url:url,
+data:demographics,
+async: true,
+dataType:'json',
+success:function(demographics){
+  console.log(demographics);
+
+  var chart = new Taucharts.Chart({
+              guide: {
+                x: {label:'Years'},  // custom label for X axis
+                y: {label:'Percentage of Population that is Asian or Pacific Islander'},    // custom label for Y axis
+                padding: {b:40,l:40,t:10,r:10},   // chart paddings
+
+              },
+              data: demographics,
+              type: 'scatterplot',
+              x: 'Year',
+              y: 'Percent Asian and Pacific Islander',
+
+              color: 'Year',
+              size:'Percent Asian and Pacific Islander',
+              plugins:[
+                Taucharts.api.plugins.get('tooltip')({
+                fields:[
+                  "Year",
+                "Two or More Races",
+                "White",
+                "Black",
+                "Percent Asian and Pacific Islander",
+                "Hispanic Origin (any race)",
+                "Other",
+                "Am. Indian, Eskimo, or Aleut"]
+              }),
+              Taucharts.api.plugins.get('legend')()
+            ]
+
+          });
+          chart.renderTo('#results');
+        }
+        });
+});
+
 //TauCharts
 
-
-
-
-
-
-
-
-
-
-
-});
+// var url = 'js/demographics-reformatted.json';
+// var race = '';
+// var year = '';
+// var percent = '';
+//
+// $.ajax({
+// type:'GET',
+// url:url,
+// data:percent,
+// async: true,
+// dataType:'json',
+// success:function(percent){
+//   console.log(percent);
+// var chart = new Taucharts.Chart({
+//
+//     type: 'stacked-bar',
+//     y: "year",
+//     x: "race",
+//     color: 'stage',
+//     data:percent,
+//
+//
+//     /*plugins:[Taucharts.api.plugins.get('tooltip')({
+//                     fields:['name', 'Infant Mortality Rate', 'Life Expectancy', 'Per Capita GDP', 'Obesity Rate', 'Unemployment Rate']
+//                   }),
+//                   Taucharts.api.plugins.get('legend')()
+//                 ]*/
+//
+//
+//
+// });
+//
+// chart.renderTo('#bar');
+// }
+// });
